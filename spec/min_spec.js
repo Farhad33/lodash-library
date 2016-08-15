5bc554648409a182f033125d25475a6f5c34493e
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-08-15 16:36:31 -0700

    finished min spec & function

--- spec/min_spec.js
+++ spec/min_spec.js
@@ -4,4 +4,8 @@ describe('min', () => {
   it ('Returns minimum value', () => {
     expect ( min ([4, 2, 8, 6])).toEqual(2)
   })
+
+  it ('Returns undefined if empty array', () => {
+    expect ( min ([])).toEqual(undefined)
+  })
 })
