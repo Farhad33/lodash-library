dd2b3b48773eb257822e296fd6230611160f0a91
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-08-15 16:45:54 -0700

    modified mid function

--- spec/min_spec.js
+++ spec/min_spec.js
@@ -2,7 +2,7 @@ import { min } from '../lib/min'
 
 describe('min', () => {
   it ('Returns minimum value', () => {
-    expect ( min ([4, 2, 8, 6])).toEqual(2)
+    expect ( min ([4, 6, 8, 2])).toEqual(2)
   })
 
   it ('Returns undefined if empty array', () => {
